d8fb14494c58cd11e31e2549bffb8d0686524938
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-12 11:24:28 +0530

    data page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DatalinksComponent } from './datalinks/datalinks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DatalinksComponent
   ],
   imports: [
     BrowserModule,
