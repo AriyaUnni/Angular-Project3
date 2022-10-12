b9c1c01285d7d44c70eebe41bfc444852c813207
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-12 11:39:00 +0530

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DatalinksComponent } from './datalinks/datalinks.component';
+import { HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { DatalinksComponent } from './datalinks/datalinks.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
